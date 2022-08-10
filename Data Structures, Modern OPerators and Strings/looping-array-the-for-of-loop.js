9274558716b1f8e7fc4584ad7940a14b4a7ac865
Robera Insarmu <92099288+rinsarmu@users.noreply.github.com>
2022-08-10 13:11:27 +0300

    Add files via upload

---
+++ Data Structures, Modern OPerators and Strings/looping-array-the-for-of-loop.js
@@ -0,0 +1,48 @@
+"use strict";
+
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23,Firenze, Italy",
+  categories: ["Italiana", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic", "Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, //OPen 24 Hours
+      close: 24,
+    },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  // orderDelivery: function (obj) {
+  //   console.log(obj);
+  // },
+  orderDelivery: function (starterIndex, address, time, mainIndex) {
+    console.log(
+      `Order is received!${
+        (this.starterMenu[starterIndex], this.mainMenu[mainIndex])
+      } will be delivered to ${address} at ${time}`
+    );
+  },
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here is your deliceous pasta with ${ing1} ${ing2} ${ing3}`);
+  },
+};
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// for (const item of menu) console.log(item);
+
+// Destructuring array on for loop
+for (const [i, j] of menu.entries()) {
+  console.log(`${[i + 1]}: ${[j]}`);
+}
